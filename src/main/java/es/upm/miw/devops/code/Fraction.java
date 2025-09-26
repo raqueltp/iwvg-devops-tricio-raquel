3e58b1fe5b600c07eeda021f9f5847d6e0f152bf
raque <r.tricio@alumnos.upm.es>
2025-09-26 03:56:39 +0200

    enhaced Fraction #7

--- src/main/java/es/upm/miw/devops/code/Fraction.java
+++ src/main/java/es/upm/miw/devops/code/Fraction.java
@@ -33,6 +33,35 @@ public class Fraction {
         return (double) numerator / denominator;
     }
 
+    public boolean isProper() {
+        return Math.abs(numerator) < Math.abs(denominator);
+    }
+
+    public boolean isImproper() {
+        return Math.abs(numerator) > Math.abs(denominator);
+    }
+
+    public boolean isEquivalent(Fraction other) {
+        return this.numerator * other.denominator == this.denominator * other.numerator;
+    }
+
+    public Fraction add(Fraction other) {
+        int commonDenominator = this.denominator * other.denominator;
+        int newNumerator = this.numerator * other.denominator + other.numerator * this.denominator;
+        return new Fraction(newNumerator, commonDenominator);
+    }
+
+    public Fraction multiply(Fraction other) {
+        return new Fraction(this.numerator * other.numerator, this.denominator * other.denominator);
+    }
+
+    public Fraction divide(Fraction other) {
+        if (other.numerator == 0) {
+            throw new ArithmeticException("Cannot divide by zero fraction");
+        }
+        return new Fraction(this.numerator * other.denominator, this.denominator * other.numerator);
+    }
+
     @Override
     public String toString() {
         return "Fraction{" +
